7b69953f51af64e25320fe6d420c9856323d62a4
tarikpnr147 <tarikpnr147@gmail.com>
2020-03-24 20:08:46 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 textOne.textContent=data.error
