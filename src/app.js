13e662cd36a6db4607e91b52b8296a3d03e1b66c
tarikpnr147 <tarikpnr147@gmail.com>
2020-03-24 20:48:44 +0300

    error has been solved

--- src/app.js
+++ src/app.js
@@ -94,7 +94,7 @@ app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
         name: 'Andrew Mead',
-        errorMessage: 'Page not found.  
+        errorMessage: 'Page not found. ' 
     })
 })
 
